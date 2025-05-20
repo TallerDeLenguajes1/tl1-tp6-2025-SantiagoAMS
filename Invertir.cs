8808050ca04bebb949f8cad85340bd462854c42d
SantiagoAMS <morales.soria.santiago.a@gmail.com>
2025-05-20 14:45:07 -0300

    Punto de invertir

---
+++ Invertir.cs
@@ -0,0 +1,22 @@
+Console.WriteLine("Ingresa el numero a invertir:");
+string s = Console.ReadLine();
+
+n = 0;
+int.TryParse(s,out n);
+
+if (n<=0){
+    Console.WriteLine("El texto ingresado no es un numero...");
+    return;
+}
+
+string ret = "";
+int dig = 0;
+
+while (n>0){
+    dig = n%10;
+    n = n/10;
+    ret = ret+dig;
+}
+
+Console.WriteLine("Numero ingresado: "+s);
+Console.WriteLine("Numero invertido: "+ret);
