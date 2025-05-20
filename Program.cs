6c0d1e9641e565eaf274144b04cc145279fb525f
SantiagoAMS <morales.soria.santiago.a@gmail.com>
2025-05-20 15:10:06 -0300

    Punto de invertir finalizado

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Ingresa el numero a invertir:");
+Console.Write("> ");
+string? s = Console.ReadLine();
+
+int n = 0;
+int.TryParse(s is null ? "" : s, out n);
+
+if (n <= 0)
+{
+    Console.WriteLine("El texto ingresado no es un numero...");
+    return;
+}
+
+string ret = "";
+int dig = 0;
+
+while (n > 0)
+{
+    dig = n % 10;
+    n = n / 10;
+    ret = ret + dig;
+}
+
+Console.WriteLine("Numero ingresado: " + s);
+Console.WriteLine("Numero invertido: " + ret);
--- Proyectos/Invertir.cs
+++ Proyectos/Invertir.cs
@@ -1,22 +1,38 @@
-Console.WriteLine("Ingresa el numero a invertir:");
-string s = Console.ReadLine();
 
-n = 0;
-int.TryParse(s,out n);
+namespace proyectos
+{
+    public class Invertir
+    {
+        public static void RealizarInversion()
+        {
+            Console.WriteLine("Ingresa el numero a invertir:");
+            string? s = Console.ReadLine();
 
-if (n<=0){
-    Console.WriteLine("El texto ingresado no es un numero...");
-    return;
+            int n = 0;
+            int.TryParse(s is null ? "" : s, out n);
+
+            if (n <= 0)
+            {
+                Console.WriteLine("El texto ingresado no es un numero...");
+                return;
+            }
+
+            string ret = "";
+            int dig = 0;
+
+            while (n > 0)
+            {
+                dig = n % 10;
+                n = n / 10;
+                ret = ret + dig;
+            }
+
+            Console.WriteLine("Numero ingresado: " + s);
+            Console.WriteLine("Numero invertido: " + ret);
+        }
+    }
+
 }
 
-string ret = "";
-int dig = 0;
 
-while (n>0){
-    dig = n%10;
-    n = n/10;
-    ret = ret+dig;
-}
 
-Console.WriteLine("Numero ingresado: "+s);
-Console.WriteLine("Numero invertido: "+ret);
