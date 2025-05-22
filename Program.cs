6d2e71fd58dd56279148a80b952744e4be4e7120
SantiagoAMS <morales.soria.santiago.a@gmail.com>
2025-05-22 10:07:14 -0300

    Comentarios

--- Program.cs
+++ Program.cs
@@ -51,11 +51,6 @@ while (opc != 0)
     }
 
 }
-static void Longitud()
-{
-
-}
-
 Console.Write("Ingresa una cadena: \n> ");
 string a = Console.ReadLine();
 a = a is null ? "" : a;
@@ -70,45 +65,46 @@ string subcadena = a.Substring(2);
 
 Console.WriteLine($"Combinada: \"{combinada}\"");
 
+
+static void Longitud()
+{
+    // Obtener la longitud de la cadena y muestre por pantalla. 
+}
+
 static void Concatenar()
 {
-
+    // A partir de una segunda cadena ingresada por el usuario, concatene ambas cadenas distintas. 
 }
 static void Subcadena()
 {
-
+    // ---● Extraer una subcadena de la cadena ingresada. 
 }
 static void CadenaForeach()
 {
-
+    // Recorrer la cadena de texto con un ciclo Foreach e ir mostrando elemento por elemento en pantalla  
 }
 
 static void Contiene()
 {
-
+    // Buscar la ocurrencia de una palabra determinada en la cadena ingresada 
 }
 static void MayMin()
 {
-
+    // Convierta la cadena a mayúsculas y luego a minúsculas. 
 }
 static void DividirSegunCaracter()
 {
-
+    // Ingrese una cadena separada por caracteres que usted determine y muestre por  pantalla los resultados (Revisar el comportamiento de split()) 
 }
 static void Calculadora()
 {
-
+    /*Siguiendo con el ejemplo de la calculadora (ejercicio 2) ingrese una ecuación 
+simple como cadena de caracteres  y que el sistema lo resuelva. Por ej. ingrese 
+por pantalla “582+2” y que le devuelva la suma de 582  con 2 */
 }
 
 
 /*
-Realizar los siguientes ejercicios  
-Dada una cadena (un string) de texto ingresada por el usuario, realice las siguientes 
-tarea:  
----● Obtener la longitud de la cadena y muestre por pantalla. 
----● A partir de una segunda cadena ingresada por el usuario, concatene ambas 
-cadenas distintas. 
----● Extraer una subcadena de la cadena ingresada. 
 ● Utilizando la calculadora creada anteriormente realizar las operaciones de dos 
 números y mostrar por pantalla y mostrar en texto el resultado. Por ejemplo para 
 la suma sería:  
@@ -116,15 +112,8 @@ la suma sería:
 Donde num1, num2 y resultados son los sumandos y el resultado de la operación 
 respectivamente. 
 Nota: Busque el comportamiento del Método ToString();  
-● Recorrer la cadena de texto con un ciclo Foreach e ir mostrando elemento por 
-elemento en pantalla  
-● Buscar la ocurrencia de una palabra determinada en la cadena ingresada 
-● Convierta la cadena a mayúsculas y luego a minúsculas. 
-● Ingrese una cadena separada por caracteres que usted determine y muestre por 
-pantalla los resultados (Revisar el comportamiento de split()) 
-● Siguiendo con el ejemplo de la calculadora (ejercicio 2) ingrese una ecuación 
-simple como cadena de caracteres  y que el sistema lo resuelva. Por ej. ingrese 
-por pantalla “582+2” y que le devuelva la suma de 582  con 2 
+
+
 Ejercicio 5. (optativo)  
 Expresiones Regulares 
 Busque en diferentes bibliografías que son y cómo funcionan las expresiones 
@@ -132,8 +121,6 @@ regulares.
 ● ¿Funcionan únicamente en C#?  
 ● ¿En qué casos le parecen útiles? Enuncie al menos 3. 
 ● ¿Cómo se hace uso de estas en C#?
-
-
 
 Resuelva las siguiente expresiones regulares
 Construir un programa que permita identificar de forma sencilla si la cadena ingresada 
