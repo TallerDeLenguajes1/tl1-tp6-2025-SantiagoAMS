3cb8b25333e80c7a266d4fbb4df82ee4b997495a
SantiagoAMS <morales.soria.santiago.a@gmail.com>
2025-05-22 10:04:54 -0300

    Base de las proximas funciones

--- Program.cs
+++ Program.cs
@@ -1,4 +1,61 @@
 ﻿
+int opc = -1;
+
+while (opc != 0)
+{
+    Console.WriteLine("==============================");
+    Console.WriteLine("  1 - Longitud");
+    Console.WriteLine("  2 - Longitud");
+    Console.WriteLine("  3 - Longitud");
+    Console.WriteLine("  4 - Longitud");
+    Console.WriteLine("  5 - Longitud");
+    Console.WriteLine("  6 - Longitud");
+    Console.WriteLine("  0 - Salir");
+    Console.WriteLine("==============================\n");
+
+    Console.Write("Ingresa una opción:\n> ");
+    opc = int.Parse(Console.ReadLine());
+
+    switch (opc)
+    {
+        case 1:
+            Longitud();
+            break;
+        case 2:
+            Concatenar();
+            break;
+        case 3:
+            Subcadena();
+            break;
+        case 4:
+            CadenaForeach();
+            break;
+        case 5:
+            Contiene();
+            break;
+        case 6:
+            MayMin();
+            break;
+        case 7:
+            DividirSegunCaracter();
+            break;
+        case 8:
+            Calculadora();
+            break;
+        case 9:
+            break;
+        case 0:
+            continue;
+        default:
+            break;
+    }
+
+}
+static void Longitud()
+{
+
+}
+
 Console.Write("Ingresa una cadena: \n> ");
 string a = Console.ReadLine();
 a = a is null ? "" : a;
@@ -7,18 +64,51 @@ Console.Write("Ingresa una segunda cadena: \n> ");
 string b = Console.ReadLine();
 b = b is null ? "" : b;
 
-Console.WriteLine($"Combinada: \"{(a+b)}\"");
+int longitud = a.Length;
+string combinada = a + b;
+string subcadena = a.Substring(2);
 
+Console.WriteLine($"Combinada: \"{combinada}\"");
+
+static void Concatenar()
+{
+
+}
+static void Subcadena()
+{
+
+}
+static void CadenaForeach()
+{
+
+}
+
+static void Contiene()
+{
+
+}
+static void MayMin()
+{
+
+}
+static void DividirSegunCaracter()
+{
+
+}
+static void Calculadora()
+{
+
+}
 
 
 /*
 Realizar los siguientes ejercicios  
 Dada una cadena (un string) de texto ingresada por el usuario, realice las siguientes 
 tarea:  
-● Obtener la longitud de la cadena y muestre por pantalla. 
-● A partir de una segunda cadena ingresada por el usuario, concatene ambas 
+---● Obtener la longitud de la cadena y muestre por pantalla. 
+---● A partir de una segunda cadena ingresada por el usuario, concatene ambas 
 cadenas distintas. 
-● Extraer una subcadena de la cadena ingresada. 
+---● Extraer una subcadena de la cadena ingresada. 
 ● Utilizando la calculadora creada anteriormente realizar las operaciones de dos 
 números y mostrar por pantalla y mostrar en texto el resultado. Por ejemplo para 
 la suma sería:  
